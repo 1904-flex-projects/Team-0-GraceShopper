66de5723e1afb4788546b428645e7f739aea0319
Ashton <asch8216@gmail.com>
2019-08-06 18:31:41 -0400

    issues with put and delete do not merge

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const Order = require('../database/models/order.js');
 const OrderItem = require('../database/models/orderItem.js');
-const Session = require('../database/models/session.js');
-const Product = require('../database/models/product');
 
 // order table and add row to orderItem
 
@@ -25,7 +23,7 @@ router.get('/:sessionId', async (req, res, next) => {
   }
 });
 
-// finding an orderItem using orderId
+// returns order based on orderId being passed in. Finds out which items belongs to cart
 router.get('/:orderId/cart', async (req, res, next) => {
   try {
     const order = await OrderItem.findAll({
@@ -40,31 +38,68 @@ router.get('/:orderId/cart', async (req, res, next) => {
 });
 
 // get route by UserId
-// router.get('/:userId/cart', (req, res, next) => {
-//   OrderItem.findAll({
-//     where: {
-//       orderId: req.params.userId,
-//     },
-//   }).then(products => res.json(products));
-// });
+router.get('/:userId/user', async (req, res, next) => {
+  try {
+    const products = await OrderItem.findAll({
+      where: {
+        orderId: req.params.userId,
+      },
+    });
+    res.json(products);
+  } catch (e) {
+    next(e);
+  }
+});
 
 // add to cart route
-router.post('/', (req, res, next) => {
-  const orderId = parseInt(req.body.orderId, 10);
-  const productId = parseInt(req.body.productId, 10);
-  const quantity = parseInt(req.body.quantity, 10);
-  const cartItem = { orderId, productId, quantity };
-  OrderItem.findOrCreate(cartItem)
-    .then(order => res.json(order))
-    .catch(next);
+router.post('/:orderId', async (req, res, next) => {
+  try {
+    // orderId, productId comes from req.body. quantity one by default
+    const orderId = parseInt(req.body.orderId, 10);
+    const productId = parseInt(req.body.productId, 10);
+    const quantity = parseInt(req.body.quantity, 10);
+    const cartItem = { orderId, productId, quantity };
+    const order = await OrderItem.findOrCreate(cartItem);
+    res.json(order);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // delete from cart route
-router.delete('/:productId', (req, res, next) => {
-  // deletiong from order items not the product table
-  Product.findByPk(req.params.productId).then(product => res.send(product.destroy()));
+router.delete('/', async (req, res, next) => {
+  try {
+    const orderId = parseInt(req.body.orderId, 10);
+    const productId = parseInt(req.body.productId, 10);
+    const item = await OrderItem.findOne({
+      where: {
+        orderId,
+        productId,
+      },
+    });
+    res.send(item.destroy());
+  } catch (e) {
+    next(e);
+  }
 });
 
-// update quantity of items in cart
+// update quantity of items in cart with put route
+
+router.put('/', async (req, res, next) => {
+  try {
+    const orderId = parseInt(req.body.orderId, 10);
+    const productId = parseInt(req.body.productId, 10);
+    const quantity = parseInt(req.body.quantity, 10);
+    const item = await OrderItem.findOne({
+      where: {
+        orderId,
+        productId,
+      },
+    });
+    res.json(item.update(quantity));
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
